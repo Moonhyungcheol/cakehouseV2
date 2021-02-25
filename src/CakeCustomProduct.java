1b6d87ba5fe29184ceae47af8cb42c866b0f56f1
Johasun <zohasun@naver.com>
2021-02-25 14:17:48 +0900

    관리자메뉴 한글입력 -> 인덱스입력으로 변경

--- src/CakeCustomProduct.java
+++ src/CakeCustomProduct.java
@@ -14,7 +14,7 @@ public class CakeCustomProduct implements Serializable {
 
 	// 품목 추가
 	/**
-	 * 
+	 * 품목추가
 	 * @param 조하선
 	 */
 	public static void productAdd(String product, String name, int price, int count) {
@@ -41,61 +41,44 @@ public class CakeCustomProduct implements Serializable {
 		}
 	}
 
+	/**
+	 * 품목제거
+	 * @param 조하선
+	 */
 	// 품목 제거
-	public static void productDel(String product, String name) {
-		int count = 0;
-		switch (product) {
-		case "Sheet":
-			for (Sheet sheet : sheetList) {
-				if (sheet.name.equals(name)) {
-					sheetList.remove(sheet);
-					System.out.println("["+name+"]" + " 제품제거에 성공하였습니다.");
-					count++;
-				}
-			}
-			if (count == 0) {
-				countCheck(name);
-			} 
-			break;
+		public static void productDel(String product, int index) {
+			switch (product) {
+			case "Sheet":
+				if((index)>CakeCustomProduct.getSheetList().size()) {
+	        		System.out.println("올바른 번호가 아닙니다. 다시 입력해주세요.");
+	        		return;
+	        	}
+				sheetList.remove(index);
+				System.out.println("제품제거에 성공하였습니다.");
+				break;
 
-		case "FreshCream":
-			for (FreshCream freshCream : freshCreamList) {
-				if (freshCream.name.equals(name)) {
-					freshCreamList.remove(freshCream);
-					System.out.println("["+name+"]" + " 제품제거에 성공하였습니다.");
-					count++;
-				}
-			}
-			if (count == 0) {
-				countCheck(name);
-			} 
-			break;
+			case "FreshCream":
+				if((index)>CakeCustomProduct.getFreshCreamList().size()) {
+	        		System.out.println("올바른 번호가 아닙니다. 다시 입력해주세요.");
+	        		return;
+	        	}
+				freshCreamList.remove(index);
+				System.out.println("제품제거에 성공하였습니다.");
+				break;
 
-		case "Topping":
-			for (Topping topping : toppingList) {
-				if (topping.name.equals(name)) {
-					toppingList.remove(topping);
-					System.out.println("["+name+"]" + " 제품제거에 성공하였습니다.");
-					count++;
-				}
-			}
-			if (count == 0) {
-				countCheck(name);
-			} 
-			break;
+			case "Topping":
+				if((index)>CakeCustomProduct.getToppingList().size()) {
+	        		System.out.println("올바른 번호가 아닙니다. 다시 입력해주세요.");
+	        		return;
+	        	}
+				toppingList.remove(index);
+				break;
 
-		default:
-			System.out.println("제품등록에 실패하였습니다.");
-			System.out.println("올바른 품목명이 아닙니다.");
-			break;
+			default:
+				System.out.println("제품제거에 실패하였습니다.");
+				break;
+			}
 		}
-	}
-
-	public static void countCheck(String str) {
-		System.out.println("해당 제품명은 존재하지 않습니다.");
-		System.out.println("확인 후 다시 이용해주세요.");
-		System.out.println("입력하신 제품명: " + str);
-	}
 
 	public static List<Sheet> getSheetList() {
 		return sheetList;
